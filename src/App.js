837350fc6ccac3f868da3f97162e82dedfb2659c
Ross <ross@sov.tech>
2019-11-14 20:27:12 +0200

    feat: connect back end to front end

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createNote } from './graphql/mutations';
 
 class App extends React.Component {
   state = {
-    notes: [{
-      id: 1,
-      note: "Hello World"
-    }]
+    note: "",
+    notes: []
+  };
+
+  handleChangeNote = event => {
+    this.setState({ note: event.target.value })
   }
+
+  handleAddNote = async event => {
+    event.preventDefault()
+    const { note, notes } = this.state;
+    const input = { note }
+    const result = await API.graphql(graphqlOperation(createNote, { input }))
+    const newNote = result.data.createNote;
+    const updatedNotes = [newNote, ...notes];
+    this.setState({ notes: updatedNotes, note: '' });
+  }
+
   render() {
-    const { notes } = this.state
+    const { notes, note } = this.state
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-1">Amplify Notetacker</h1>
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="Write a note"/>
+        <form onSubmit={this.handleAddNote} className="mb3">
+          <input type="text" className="pa2 f4" placeholder="Write a note" onChange={this.handleChangeNote}
+          value={note} />
           <button className="pa2 f4" type="submit">
             Add Note
           </button>
